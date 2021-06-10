50f6a8bf49da96e6bcfde70da4f04c8954decb57
Guzel <82291811+badretdinovaG@users.noreply.github.com>
2021-06-11 01:42:45 +0300

    Delete bmp.h

--- lab2/bmp.h
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#pragma once
-#include <stdio.h>
-#include <windows.h>
-#define WIN32_MEAN_AND_LEAN
-
-void read(const char* file_in, const char* file_out);
-void out(BITMAPFILEHEADER file_header, BITMAPINFOHEADER info_header, FILE* input, FILE* output);
